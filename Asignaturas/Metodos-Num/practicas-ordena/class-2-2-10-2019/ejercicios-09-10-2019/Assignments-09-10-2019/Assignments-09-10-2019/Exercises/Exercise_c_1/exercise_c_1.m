0d7623e4fec911b3222448ab8a499431389bd80f
Josu Pérez Zarraonandia <josu.perez.zarra@gmail.com>
2019-10-10 09:48:06 +0200

    Corregida carpeta de practicas 2 de Metodos

--- Asignaturas/Metodos-Num/practicas-ordena/class-2-2-10-2019/ejercicios-09-10-2019/Assignments-09-10-2019/Assignments-09-10-2019/Exercises/Exercise_c_1/exercise_c_1.m
+++
@@ -1,9 +0,0 @@
-% GROUP 15: Josu Pérez Zarraonandia & Ander Chocarro Salaberria
-
-% ODE exercise 1: Part 2
-
-[x, y]=ode45(@ode_1,[0,10],5);
-plot(x,y(:,1),'-o')
-xlabel('\it x axis')
-ylabel('\it y axis')
-title('Solution of the ODE: y''(x) + y(x) = 3cos(x)')
